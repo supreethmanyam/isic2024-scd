d72d960c2bc6831e0f1a6f96b7549b2adb172de2
Supreeth Manyam <supreethmanyam@gmail.com>
2024-07-08 18:06:48 -0700

    Kaggle Notebook | isic_helper.py | Version 1

---
+++ isic-helper-py.py
@@ -0,0 +1,133 @@
+from typing import Any
+from argparse import Namespace
+import typing
+
+import numpy as np
+import pandas as pd
+import torch
+
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_auc_score as compute_auc
+
+
+class DotDict(Namespace):
+    """A simple class that builds upon `argparse.Namespace`
+    in order to make chained attributes possible."""
+
+    def __init__(self, temp=False, key=None, parent=None) -> None:
+        self._temp = temp
+        self._key = key
+        self._parent = parent
+
+    def __eq__(self, other):
+        if not isinstance(other, DotDict):
+            return NotImplemented
+        return vars(self) == vars(other)
+
+    def __getattr__(self, __name: str) -> Any:
+        if __name not in self.__dict__ and not self._temp:
+            self.__dict__[__name] = DotDict(temp=True, key=__name, parent=self)
+        else:
+            del self._parent.__dict__[self._key]
+            raise AttributeError("No attribute '%s'" % __name)
+        return self.__dict__[__name]
+
+    def __repr__(self) -> str:
+        item_keys = [k for k in self.__dict__ if not k.startswith("_")]
+
+        if len(item_keys) == 0:
+            return "DotDict()"
+        elif len(item_keys) == 1:
+            key = item_keys[0]
+            val = self.__dict__[key]
+            return "DotDict(%s=%s)" % (key, repr(val))
+        else:
+            return "DotDict(%s)" % ", ".join(
+                "%s=%s" % (key, repr(val)) for key, val in self.__dict__.items()
+            )
+
+    @classmethod
+    def from_dict(cls, original: typing.Mapping[str, any]) -> "DotDict":
+        """Create a DotDict from a (possibly nested) dict `original`.
+        Warning: this method should not be used on very deeply nested inputs,
+        since it's recursively traversing the nested dictionary values.
+        """
+        dd = DotDict()
+        for key, value in original.items():
+            if isinstance(value, typing.Mapping):
+                value = cls.from_dict(value)
+            setattr(dd, key, value)
+        return dd
+
+
+def compute_pauc(y_true, y_pred, min_tpr: float=0.80) -> float:
+    '''
+    2024 ISIC Challenge metric: pAUC
+    
+    Given a solution file and submission file, this function returns the
+    the partial area under the receiver operating characteristic (pAUC) 
+    above a given true positive rate (TPR) = 0.80.
+    https://en.wikipedia.org/wiki/Partial_Area_Under_the_ROC_Curve.
+    
+    (c) 2024 Nicholas R Kurtansky, MSKCC
+
+    Args:
+        y_true: ground truth of 1s and 0s
+        y_pred: predictions of scores ranging [0, 1]
+
+    Returns:
+        Float value range [0, max_fpr]
+    '''
+
+    # rescale the target. set 0s to 1s and 1s to 0s (since sklearn only has max_fpr)
+    v_gt = abs(y_true-1)
+    
+    # flip the submissions to their compliments
+    v_pred = -1.0 * y_pred
+
+    max_fpr = abs(1 - min_tpr)
+
+    # using sklearn.metric functions: (1) roc_curve and (2) auc
+    fpr, tpr, _ = roc_curve(v_gt, v_pred, sample_weight=None)
+    if max_fpr is None or max_fpr == 1:
+        return auc(fpr, tpr)
+    if max_fpr <= 0 or max_fpr > 1:
+        raise ValueError("Expected min_tpr in range [0, 1), got: %r" % min_tpr)
+        
+    # Add a single point at max_fpr by linear interpolation
+    stop = np.searchsorted(fpr, max_fpr, "right")
+    x_interp = [fpr[stop - 1], fpr[stop]]
+    y_interp = [tpr[stop - 1], tpr[stop]]
+    tpr = np.append(tpr[:stop], np.interp(max_fpr, x_interp, y_interp))
+    fpr = np.append(fpr[:stop], max_fpr)
+    partial_auc = auc(fpr, tpr)
+
+#     # Equivalent code that uses sklearn's roc_auc_score
+#     v_gt = abs(np.asarray(solution.values)-1)
+#     v_pred = np.array([1.0 - x for x in submission.values])
+#     max_fpr = abs(1-min_tpr)
+#     partial_auc_scaled = roc_auc_score(v_gt, v_pred, max_fpr=max_fpr)
+#     # change scale from [0.5, 1.0] to [0.5 * max_fpr**2, max_fpr]
+#     # https://math.stackexchange.com/questions/914823/shift-numbers-into-a-different-range
+#     partial_auc = 0.5 * max_fpr**2 + (max_fpr - 0.5 * max_fpr**2) / (1.0 - 0.5) * (partial_auc_scaled - 0.5)
+    
+    return(partial_auc)
+
+    
+def get_folds():
+    folds_df = pd.read_csv("/kaggle/input/isic-scd-artifacts/folds.csv")
+    return folds_df
+
+
+def time_to_str(duration, unit='sec'):
+    if unit == 'min':
+        minutes = int(duration // 60)
+        seconds = duration % 60
+        return f"{minutes} min {seconds:.2f} sec"
+    else:
+        return f"{duration:.2f} sec"
+
+def set_seed(seed=0):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
