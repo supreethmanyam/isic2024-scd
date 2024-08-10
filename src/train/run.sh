df9bba6c382daa0460ff7463e92cf7290ae70895
supreethmanyam <supreethmanyam@gmail.com>
2024-08-10 00:16:21 -0700

    resnet50:v1 Full run

--- src/train/run.sh
+++ src/train/run.sh
@@ -1,11 +1,11 @@
-modal run -d isic2024_scd_app.py::train --model-name mobilevitv2_200 --version v1 --fold 1
-#Link: https://modal.com/apps/supreeth-manyam/main/ap-t1kS3mPfXenaeHEGHy2u05
-modal run -d isic2024_scd_app.py::train --model-name mobilevitv2_200 --version v1 --fold 2
-#Link: https://modal.com/apps/supreeth-manyam/main/ap-zSgWaMF2n0ziITpXnp46vc
-modal run -d isic2024_scd_app.py::train --model-name mobilevitv2_200 --version v1 --fold 3
-#Link: https://modal.com/apps/supreeth-manyam/main/ap-BvTR5DJucPnQAEmTnzFkOZ
-modal run -d isic2024_scd_app.py::train --model-name mobilevitv2_200 --version v1 --fold 4
-#Link: https://modal.com/apps/supreeth-manyam/main/ap-001XjXuUZJpo4VaVd7d7d0
-modal run -d isic2024_scd_app.py::train --model-name mobilevitv2_200 --version v1 --fold 5
-#Link: https://modal.com/apps/supreeth-manyam/main/ap-fCDZREElqFhaIjxiFfTQND
-modal run -d isic2024_scd_app.py::upload_weights --model-name mobilevitv2_200 --version v1
+modal run -d isic2024_scd_app.py::train --model-name resnet50 --version v1 --fold 1
+#Link: https://modal.com/apps/supreeth-manyam/main/ap-XEaAcyhTHED0RMtObJ0W98
+modal run -d isic2024_scd_app.py::train --model-name resnet50 --version v1 --fold 2
+#Link: https://modal.com/apps/supreeth-manyam/main/ap-zczekhn5k7CdwjiV4kNQqy
+modal run -d isic2024_scd_app.py::train --model-name resnet50 --version v1 --fold 3
+#Link: https://modal.com/apps/supreeth-manyam/main/ap-ZNBsBZMF3Uvi7b0eCSfJ9X
+modal run -d isic2024_scd_app.py::train --model-name resnet50 --version v1 --fold 4
+#Link: https://modal.com/apps/supreeth-manyam/main/ap-Uh3iJlomC9nbXL2wITOYnE
+modal run -d isic2024_scd_app.py::train --model-name resnet50 --version v1 --fold 5
+#Link: https://modal.com/apps/supreeth-manyam/main/ap-hDh2VuIdXh43EuRKW7RMc3
+modal run -d isic2024_scd_app.py::upload_weights --model-name resnet50 --version v1
