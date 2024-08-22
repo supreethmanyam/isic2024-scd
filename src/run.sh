bbb84e93b2d1989913eddcdcd666ee8a076a3959
supreethmanyam <supreethmanyam@gmail.com>
2024-08-22 12:21:42 -0700

    seresnet50_v1

--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,19 @@
 modal run -d isic2024_scd_app.py::download_data
 
-modal run -d isic2024_scd_app.py::pretrain --model-name mobilevitv2_200 --version v1 --fold 1
-#Link: https://modal.com/apps/supreethmanyam/main/ap-u2Y7NirE7RqDxkslqqc0x2
-modal run -d isic2024_scd_app.py::pretrain --model-name mobilevitv2_200 --version v1 --fold 2
-#Link: https://modal.com/apps/supreethmanyam/main/ap-bkQbwIz96iD7iAuWTAECuu
-modal run -d isic2024_scd_app.py::pretrain --model-name mobilevitv2_200 --version v1 --fold 3
-#Link: https://modal.com/apps/supreethmanyam/main/ap-d5CD0AJ9m2creQzDvJKBgd
-modal run -d isic2024_scd_app.py::pretrain --model-name mobilevitv2_200 --version v1 --fold 4
-#Link: https://modal.com/apps/supreethmanyam/main/ap-tUI4Xzk22oqg45NKpz241b
-modal run -d isic2024_scd_app.py::pretrain --model-name mobilevitv2_200 --version v1 --fold 5
-#Link: https://modal.com/apps/supreethmanyam/main/ap-YBcNFi0cX9kTYOHczrA4Y5
-modal run -d isic2024_scd_app.py::upload_weights --model-name mobilevitv2_200 --version v1 --mode pretrain
+modal run -d isic2024_scd_app.py::pretrain --model-name seresnet50 --version v1 --fold 1
+#Link: https://modal.com/apps/supreethmanyam/main/ap-HPCUfp93i17440yjukB1YO
+modal run -d isic2024_scd_app.py::pretrain --model-name seresnet50 --version v1 --fold 2
+#Link: https://modal.com/apps/supreethmanyam/main/ap-LPUXKsVovw0AiRMpc5lhbA
+modal run -d isic2024_scd_app.py::pretrain --model-name seresnet50 --version v1 --fold 3
+#Link: https://modal.com/apps/supreethmanyam/main/ap-ELWdPLOpNc7nRWILo9IIVb
+modal run -d isic2024_scd_app.py::pretrain --model-name seresnet50 --version v1 --fold 4
+#Link: https://modal.com/apps/supreethmanyam/main/ap-Pdzabs2BsIFJVQEh8F5gbF
+modal run -d isic2024_scd_app.py::pretrain --model-name seresnet50 --version v1 --fold 5
+#Link: https://modal.com/apps/supreethmanyam/main/ap-CAdJUFpjGgESEkLN8wVvXA
+modal run -d isic2024_scd_app.py::upload_weights --model-name seresnet50 --version v1 --mode pretrain
+
+
+#modal run -d isic2024_scd_app.py::pretrain --model-name tf_efficientnetv2_s --version v1 --fold 1
+##Link: https://modal.com/apps/supreethmanyam/main/ap-N0hh3dj2LRb5qqtRtV8m3J
+#modal run -d isic2024_scd_app.py::pretrain --model-name tf_efficientnet_b4 --version v1 --fold 1
+##Link: https://modal.com/apps/supreethmanyam/main/ap-Lk82o7VMD2sR245JuxKOjP
