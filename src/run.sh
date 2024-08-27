3b8d242a9e9917751ebb81bb227ce4d9fb883cd1
supreethmanyam <supreethmanyam@gmail.com>
2024-08-26 23:05:36 -0700

    resnet18_v1:fullrun

--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,13 @@
 modal run -d isic2024_scd_app.py::download_data
 
-modal run -d isic2024_scd_app.py::pretrain --model-name efficientnet_b2 --version v1 --fold 1
-#Link: https://modal.com/apps/supreethmanyam/main/ap-VPSc8YTJ3HIMeQLFfC9FgG
-modal run -d isic2024_scd_app.py::pretrain --model-name efficientnet_b2 --version v1 --fold 2
-#Link: https://modal.com/apps/supreethmanyam/main/ap-r8fdtpxCVEHSAhH1TiimE2
-modal run -d isic2024_scd_app.py::pretrain --model-name efficientnet_b2 --version v1 --fold 3
-#Link: https://modal.com/apps/supreethmanyam/main/ap-3UiBtxZBchpjFS98BmdKKr
-modal run -d isic2024_scd_app.py::pretrain --model-name efficientnet_b2 --version v1 --fold 4
-#Link: https://modal.com/apps/supreethmanyam/main/ap-Rrbcb0Wo29QyU6p7ytt9mQ
-modal run -d isic2024_scd_app.py::pretrain --model-name efficientnet_b2 --version v1 --fold 5
-#Link: https://modal.com/apps/supreethmanyam/main/ap-SOclLWJ8Clm4vEKhumAtVw
-modal run -d isic2024_scd_app.py::upload_weights --model-name efficientnet_b2 --version v1 --mode pretrain
+modal run -d isic2024_scd_app.py::pretrain --model-name resnet18 --version v1 --fold 1
+#Link: https://modal.com/apps/supreethmanyam/main/ap-rbafQ2YaFwmolztToLXGD7
+modal run -d isic2024_scd_app.py::pretrain --model-name resnet18 --version v1 --fold 2
+#Link: https://modal.com/apps/supreethmanyam/main/ap-xUpQ47gIv0LO0StBG7rQri
+modal run -d isic2024_scd_app.py::pretrain --model-name resnet18 --version v1 --fold 3
+#Link: https://modal.com/apps/supreethmanyam/main/ap-RjNOWdF64m8gTqVH1t4NL3
+modal run -d isic2024_scd_app.py::pretrain --model-name resnet18 --version v1 --fold 4
+#Link: https://modal.com/apps/supreethmanyam/main/ap-hwt1Su2sqnAoWaFqbjOOY9
+modal run -d isic2024_scd_app.py::pretrain --model-name resnet18 --version v1 --fold 5
+#Link: https://modal.com/apps/supreethmanyam/main/ap-xjya6ICZfh691oj77hXoU8
+modal run -d isic2024_scd_app.py::upload_weights --model-name resnet18 --version v1 --mode pretrain
