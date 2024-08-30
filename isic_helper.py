b45c5c4845fe0d4e9f9a7385b6a0a342cce020dd
Supreeth Manyam <supreethmanyam@gmail.com>
2024-08-29 18:31:15 -0700

    Kaggle Notebook | isic_helper | Version 1

--- isic_helper.py
+++ isic_helper.py
@@ -1,17 +1,16 @@
+# %% [code]
 from typing import Any
 from argparse import Namespace
 import typing
 
 import numpy as np
-import pandas as pd
-import torch
 
 from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import roc_auc_score as compute_auc
 
 
-def time_to_str(duration, unit='sec'):
-    if unit == 'min':
+def time_to_str(duration, unit='min'):
+    if unit == "min":
         minutes = int(duration // 60)
         seconds = duration % 60
         return f"{minutes} min {seconds:.2f} sec"
@@ -19,39 +18,29 @@ def time_to_str(duration, unit='sec'):
         return f"{duration:.2f} sec"
 
 
-def get_folds():
-    folds_df = pd.read_csv("/kaggle/input/isic-scd-folds/folds.csv")
-    return folds_df
-
-
-def set_seed(seed=0):
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    
-
-def compute_pauc(y_true, y_pred, min_tpr: float=0.80) -> float:
-    '''
+def compute_pauc(y_true, y_pred, min_tpr: float = 0.80) -> float:
+    """
     2024 ISIC Challenge metric: pAUC
-    
+
     Given a solution file and submission file, this function returns the
-    the partial area under the receiver operating characteristic (pAUC) 
+    partial area under the receiver operating characteristic (pAUC)
     above a given true positive rate (TPR) = 0.80.
     https://en.wikipedia.org/wiki/Partial_Area_Under_the_ROC_Curve.
-    
+
     (c) 2024 Nicholas R Kurtansky, MSKCC
 
     Args:
+        min_tpr: minimum true positive rate
         y_true: ground truth of 1s and 0s
         y_pred: predictions of scores ranging [0, 1]
 
     Returns:
         Float value range [0, max_fpr]
-    '''
+    """
 
     # rescale the target. set 0s to 1s and 1s to 0s (since sklearn only has max_fpr)
-    v_gt = abs(y_true-1)
-    
+    v_gt = abs(y_true - 1)
+
     # flip the submissions to their compliments
     v_pred = -1.0 * y_pred
 
@@ -63,7 +52,7 @@ def compute_pauc(y_true, y_pred, min_tpr: float=0.80) -> float:
         return auc(fpr, tpr)
     if max_fpr <= 0 or max_fpr > 1:
         raise ValueError("Expected min_tpr in range [0, 1), got: %r" % min_tpr)
-        
+
     # Add a single point at max_fpr by linear interpolation
     stop = np.searchsorted(fpr, max_fpr, "right")
     x_interp = [fpr[stop - 1], fpr[stop]]
@@ -72,23 +61,24 @@ def compute_pauc(y_true, y_pred, min_tpr: float=0.80) -> float:
     fpr = np.append(fpr[:stop], max_fpr)
     partial_auc = auc(fpr, tpr)
 
-#     # Equivalent code that uses sklearn's roc_auc_score
-#     v_gt = abs(np.asarray(solution.values)-1)
-#     v_pred = np.array([1.0 - x for x in submission.values])
-#     max_fpr = abs(1-min_tpr)
-#     partial_auc_scaled = roc_auc_score(v_gt, v_pred, max_fpr=max_fpr)
-#     # change scale from [0.5, 1.0] to [0.5 * max_fpr**2, max_fpr]
-#     # https://math.stackexchange.com/questions/914823/shift-numbers-into-a-different-range
-#     partial_auc = 0.5 * max_fpr**2 + (max_fpr - 0.5 * max_fpr**2) / (1.0 - 0.5) * (partial_auc_scaled - 0.5)
-    
-    return(partial_auc)
+    #     # Equivalent code that uses sklearn's roc_auc_score
+    #     v_gt = abs(np.asarray(solution.values)-1)
+    #     v_pred = np.array([1.0 - x for x in submission.values])
+    #     max_fpr = abs(1-min_tpr)
+    #     partial_auc_scaled = roc_auc_score(v_gt, v_pred, max_fpr=max_fpr)
+    #     # change scale from [0.5, 1.0] to [0.5 * max_fpr**2, max_fpr]
+    #     # https://math.stackexchange.com/questions/914823/shift-numbers-into-a-different-range
+    #     partial_auc = 0.5 * max_fpr**2 + (max_fpr - 0.5 * max_fpr**2) / (1.0 - 0.5) * (partial_auc_scaled - 0.5)
+
+    return partial_auc
 
 
 class DotDict(Namespace):
     """A simple class that builds upon `argparse.Namespace`
     in order to make chained attributes possible."""
 
-    def __init__(self, temp=False, key=None, parent=None) -> None:
+    def __init__(self, temp=False, key=None, parent=None, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
         self._temp = temp
         self._key = key
         self._parent = parent
