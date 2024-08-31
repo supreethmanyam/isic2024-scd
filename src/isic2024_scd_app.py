bb28928dd5b97f0338478257de770ec8d5b7879f
supreethmanyam <supreethmanyam@gmail.com>
2024-08-30 17:09:45 -0700

    efficientnet_b1_v2:trainbinary:UsingPretrainedWeightsFromMulti:FullRun

--- src/isic2024_scd_app.py
+++ src/isic2024_scd_app.py
@@ -348,7 +348,7 @@ class TrainBinaryConfig:
     mode: str = "trainbinary"
     fold_column: str = "gkf_fold"
     mixed_precision: bool = "fp16"
-    image_size: int = 64
+    image_size: int = 92
     train_batch_size: int = 64
     val_batch_size: int = 512
     num_workers: int = 8
--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,13 @@
 modal run -d isic2024_scd_app.py::download_data
 
-modal run -d isic2024_scd_app.py::trainbinary --model-name efficientnet_b0 --version v2 --fold 1
-#Link: https://modal.com/apps/supreethmanyam/main/ap-2Mh0r73JNaAQfxL7TCq5QH
-modal run -d isic2024_scd_app.py::trainbinary --model-name efficientnet_b0 --version v2 --fold 2
-#Link: https://modal.com/apps/supreethmanyam/main/ap-3zwTFfx1jLTC3gBFwTPtW9
-modal run -d isic2024_scd_app.py::trainbinary --model-name efficientnet_b0 --version v2 --fold 3
-#Link: https://modal.com/apps/supreethmanyam/main/ap-Suehj25EuRzmDWortpWghd
-modal run -d isic2024_scd_app.py::trainbinary --model-name efficientnet_b0 --version v2 --fold 4
-#Link: https://modal.com/apps/supreethmanyam/main/ap-b1pHyIoCrmYnvdleKv0Vqv
-modal run -d isic2024_scd_app.py::trainbinary --model-name efficientnet_b0 --version v2 --fold 5
-#Link: https://modal.com/apps/supreethmanyam/main/ap-UqAPTUMPPhjK7q472HpBdQ
-modal run -d isic2024_scd_app.py::upload_weights --model-name efficientnet_b0 --version v2 --mode trainbinary
+modal run -d isic2024_scd_app.py::trainbinary --model-name efficientnet_b1 --version v2 --fold 1
+#Link: https://modal.com/apps/supreethmanyam/main/ap-o68SnUk9sjczd7UvRpqk8c
+modal run -d isic2024_scd_app.py::trainbinary --model-name efficientnet_b1 --version v2 --fold 2
+#Link: https://modal.com/apps/supreethmanyam/main/ap-n5pGHeImZSyQCdP9trGrjf
+modal run -d isic2024_scd_app.py::trainbinary --model-name efficientnet_b1 --version v2 --fold 3
+#Link: https://modal.com/apps/supreethmanyam/main/ap-eumz9cl8GQJdWoKpISPF12
+modal run -d isic2024_scd_app.py::trainbinary --model-name efficientnet_b1 --version v2 --fold 4
+#Link: https://modal.com/apps/supreethmanyam/main/ap-IvJS6HHgBHupN6TzdCUY3n
+modal run -d isic2024_scd_app.py::trainbinary --model-name efficientnet_b1 --version v2 --fold 5
+#Link: https://modal.com/apps/supreethmanyam/main/ap-YGMXwg7hX4H5vEt4qTZC45
+modal run -d isic2024_scd_app.py::upload_weights --model-name efficientnet_b1 --version v2 --mode trainbinary
